65e9568728cad22a72287b5bf5439c8fd5b6898b
Lawrence Hunt <lawrence.hunt@gmail.com>
2014-05-24 14:21:10 +0100

    Added angular to require config

--- js/init.js
+++ js/init.js
@@ -2,6 +2,9 @@ require.config({
     deps: ['main'],
 
     shim: {
+        angular: {
+            exports: 'angular'
+        },
         underscore: {
             exports: '_'
         },
@@ -20,6 +23,7 @@ require.config({
         underscore: '../bower_components/underscore/underscore',
         backbone: '../bower_components/backbone/backbone',
         marionette: '../bower_components/marionette/lib/backbone.marionette',
-        'backbone.wreqr': '../bower_components/backbone.wreqr/lib/backbone.wreqr'
+        'backbone.wreqr': '../bower_components/backbone.wreqr/lib/backbone.wreqr',
+        angular: '../bower_components/angular/angular'
     }
 });
