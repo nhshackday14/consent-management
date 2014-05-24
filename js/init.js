96170ad44f4dab9406c85ac2db392b9520ece1a0
Lawrence Hunt <lawrence.hunt@gmail.com>
2014-05-24 14:59:26 +0100

    Removed backbone :( Added Angular :(

--- js/init.js
+++ js/init.js
@@ -7,23 +7,12 @@ require.config({
         },
         underscore: {
             exports: '_'
-        },
-        backbone: {
-            deps: ['jquery', 'underscore'],
-            exports: 'Backbone'
-        },
-        marionette: {
-            exports: 'Backbone.Marionette',
-            deps: ['backbone']
         }
     },
 
     paths: {
         jquery: '../bower_components/jquery/dist/jquery',
         underscore: '../bower_components/underscore/underscore',
-        backbone: '../bower_components/backbone/backbone',
-        marionette: '../bower_components/marionette/lib/backbone.marionette',
-        'backbone.wreqr': '../bower_components/backbone.wreqr/lib/backbone.wreqr',
         angular: '../bower_components/angular/angular'
     }
 });
