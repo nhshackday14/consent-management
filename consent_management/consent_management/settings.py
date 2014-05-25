6148c9051cddeb885cf6f887af8edfadea778817
Davide Ceretti <davide.ceretti@hogarthww.com>
2014-05-25 11:08:26 +0100

    add public folder

--- consent_management/consent_management/settings.py
+++ consent_management/consent_management/settings.py
@@ -82,3 +82,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, '../public'),
+)
