ce1eceae17acfcc14edf1b26849a00eb55dd12a2
Fred Kingham <fred@potatolondon.com>
2014-05-26 14:32:31 +0100

    added fabfile with bower install commands

---
+++ fabfile.py
@@ -0,0 +1,25 @@
+from fabric.api import env
+from fabric.contrib.project import rsync_project
+from fabric.api import run
+from fabric.context_managers import cd
+env.key_filename = "../../nother.pem"
+env.hosts = ["54.247.69.211"]
+env.user = "ubuntu"
+
+
+def rsync_to_prod():
+    rsync_settings = {"remote_dir": "/home/ubuntu"}
+    rsync_settings["delete"] = True
+    rsync_settings["exclude"] = ["*.pyc", "*.git", "fabfile.pyi", "*.pdf"]
+    rsync_project(**rsync_settings)
+
+
+def install_bower():
+    with cd("consentmanagement"):
+        run("npm install")
+        run("bower install")
+
+
+def push_to_prod():
+    rsync_to_prod()
+    install_bower()
