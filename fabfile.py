0e780059c9925fb458d23ffb498cb925b2451bfd
Fred Kingham <fred@potatolondon.com>
2014-06-07 12:35:13 +0100

    added in a db dump in the fabfile, requirements update and south migrate

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,11 @@
 from fabric.api import env, lcd, run, local
 from fabric.contrib.project import rsync_project
-from fabric.context_managers import cd
+from fabric.context_managers import cd, prefix
 from fabric.operations import put
 env.key_filename = "../../nother.pem"
 env.hosts = ["54.247.69.211"]
 env.user = "ubuntu"
+app_name = "consent-management"
 
 
 def rsync_to_prod():
@@ -29,13 +30,38 @@ def upload_settings():
     )
 
 
+def workon():
+        return "source /usr/local/bin/virtualenvwrapper.sh && workon %s" % app_name
+
+
+def install_requirements():
+    with prefix(workon()):
+        with cd("consent-management"):
+            run("pip install -r requirements.txt")
+
+
 def collect_static():
     with lcd("consent_management"):
-        local("python manage.py collectstatic")
+        local("python manage.py collectstatic --noinput")
 
+
+def sync_db():
+    with prefix(workon()):
+        with cd("consent-management/consent_management"):
+            run("./manage.py syncdb")
+            run("./manage.py migrate")
+
+
+def db_dump():
+    with prefix(workon()):
+        with cd("consent-management/consent_management"):
+            run("./manage.py dumpdata > ../datadump.json")
 
 def push_to_prod():
     build_static()
     collect_static()
     rsync_to_prod()
+    db_dump()
+    install_requirements()
+    sync_db()
     upload_settings()
